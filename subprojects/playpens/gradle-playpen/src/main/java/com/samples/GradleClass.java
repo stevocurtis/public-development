6cd526e744ac210c4ca48bf753ef4c5e9da7d27c
steven.curtis@gmail.com <steven.curtis@gmail.com>
2015-02-19 11:57:31 +0000

    Used Arrays.toString to print the array

--- subprojects/playpens/gradle-playpen/src/main/java/com/samples/GradleClass.java
+++ subprojects/playpens/gradle-playpen/src/main/java/com/samples/GradleClass.java
@@ -3,6 +3,8 @@ package com.samples;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 public class GradleClass
 {
     private static Logger logger = LoggerFactory.getLogger(GradleClass.class);
@@ -14,7 +16,7 @@ public class GradleClass
 
     public int run(String[] args)
     {
-        logger.info("Received args: {}", args);
+        logger.info("Received args: {}", Arrays.toString(args));
         return (args == null ? 0 : args.length);
     }
 }
