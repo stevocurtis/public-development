cac8ddd4a7a76989f149b5e23f6d92e71027f2e1
steven.curtis@gmail.com <steven.curtis@gmail.com>
2015-02-20 10:19:14 +0000

    Added JCSV library.

---
+++ subprojects/playpens/java-playpen/jcsv-playpen/src/main/java/com/fenixinfotech/jcsv/playpen/CSVReader.java
@@ -0,0 +1,42 @@
+package com.fenixinfotech.jcsv.playpen;
+
+import com.googlecode.jcsv.CSVStrategy;
+import com.googlecode.jcsv.annotations.internal.ValueProcessorProvider;
+import com.googlecode.jcsv.reader.internal.AnnotationEntryParser;
+import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVReader
+{
+    private static final Logger logger = LoggerFactory.getLogger(CSVReader.class);
+
+    public void readCSV(File csvFile)
+    {
+        logger.info("readCSV with csvFile {}", csvFile);
+
+        Reader reader;
+        List<CSVPojo> csvPojos = new ArrayList<CSVPojo>();
+        try
+        {
+            reader = new FileReader(csvFile);
+            ValueProcessorProvider vpp = new ValueProcessorProvider();
+            com.googlecode.jcsv.reader.CSVReader<CSVPojo> csvReader = new CSVReaderBuilder<CSVPojo>(reader).strategy(CSVStrategy.UK_DEFAULT).entryParser(new AnnotationEntryParser<CSVPojo>(CSVPojo.class, vpp)).build();
+            csvPojos = csvReader.readAll();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        logger.info("readCSV with csvFile {} found csvPojos {}", csvFile, csvPojos);
+    }
+}
