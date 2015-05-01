0a4c7cfda0c4ac3c87bf75b68ebfc6595e75b7f7
steven.curtis@gmail.com <steven.curtis@gmail.com>
2015-05-01 17:29:51 +0100

    Corrected module name for input parameter

--- subprojects/cloudify/poc/poc-module/bin/deployBlueprint.sh
+++ subprojects/cloudify/poc/poc-module/bin/deployBlueprint.sh
@@ -6,7 +6,7 @@ export dep="dep-$1"
 cfy blueprints upload -p poc-module/blueprints/$1.yaml -b ${bp}
 echo upload upload status is $?
 
-cfy deployments create -d $dep -b $bp -i poc-module-new/blueprints/inputs/$1.json
+cfy deployments create -d $dep -b $bp -i poc-module/blueprints/inputs/$1.json
 echo deployments create status is $?
 
 echo cfy executions list -d $dep | grep start
