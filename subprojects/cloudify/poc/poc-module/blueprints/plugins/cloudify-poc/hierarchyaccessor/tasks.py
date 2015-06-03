15c38b0918651aa81aa218a017f72311ba04552b
steven.curtis@gmail.com <steven.curtis@gmail.com>
2015-06-03 13:01:08 +0100

    Corrected shared cloud router node type and updated hierarchy library to dump node properties and node instance runtime properties

--- subprojects/cloudify/poc/poc-module/blueprints/plugins/cloudify-poc/hierarchyaccessor/tasks.py
+++ subprojects/cloudify/poc/poc-module/blueprints/plugins/cloudify-poc/hierarchyaccessor/tasks.py
@@ -8,6 +8,10 @@ def addhierarchyruntimeproperties(**kwargs):
     ctx.logger.info("*** executing addhierarchyruntimeproperties ***")
 
     for node in ctx.nodes:
-        ctx.logger.info("*** found node {} ***".format(node))
+        nodeProperties = node.properties
+        ctx.logger.info("*** node with id {} and name {} has properties {}***".format(node.id, node.name, nodeProperties))
+        for instance in node.instances:
+            nodeInstanceRuntimeProperties = node.runtime_properties
+            ctx.logger.info("   *** node instance with {} has properties {}***".format(node, nodeInstanceRuntimeProperties))
 
     ctx.logger.info("*** completed addhierarchyruntimeproperties ***")
