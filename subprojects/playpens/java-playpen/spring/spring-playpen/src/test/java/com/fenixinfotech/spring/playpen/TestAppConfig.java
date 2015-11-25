8af5fea35b1b83288ea655e49cd51188706e2fdc
steven.curtis@gmail.com <steven.curtis@gmail.com>
2015-11-25 20:51:57 +0000

    Added test app config

---
+++ subprojects/playpens/java-playpen/spring/spring-playpen/src/test/java/com/fenixinfotech/spring/playpen/TestAppConfig.java
@@ -0,0 +1,11 @@
+package com.fenixinfotech.spring.playpen;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fenixinfotech")
+public class TestAppConfig
+{
+
+}
