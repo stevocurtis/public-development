bdbbfed1d0cb19f36345f4717134dbb1d7222747
steven.curtis@gmail.com <steven.curtis@gmail.com>
2016-02-19 11:40:37 +0000

    Removed unused imports

--- subprojects/playpens/java-playpen/spring/spring-playpen/src/main/java/com/fenixinfotech/spring/playpen/services/SpringDataService.java
+++ subprojects/playpens/java-playpen/spring/spring-playpen/src/main/java/com/fenixinfotech/spring/playpen/services/SpringDataService.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
