aacc21a1311b053bb18b5a59d1322aef48e287a7
stevocurtis <stevo.curtis@gmail.com>
2016-06-07 23:10:44 +0100

    Added jersey based grizzly server

---
+++ subprojects/playpens/java-playpen/web/grizzly-framework-playpen/src/main/java/com/fenixinfotech/grizzly/framework/playpen/JerseyGrizzlyFrameworkServer.java
@@ -0,0 +1,122 @@
+package com.fenixinfotech.grizzly.framework.playpen;
+
+import com.fenixinfotech.web.common.FrameworkServerBase;
+import com.fenixinfotech.web.common.JerseyResource;
+import org.glassfish.grizzly.http.server.*;
+import org.glassfish.grizzly.nio.NIOTransport;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
+import org.glassfish.grizzly.strategies.WorkerThreadIOStrategy;
+import org.glassfish.grizzly.threadpool.GrizzlyExecutorService;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
+{
+    private static final Logger logger = LoggerFactory.getLogger(JerseyGrizzlyFrameworkServer.class);
+
+    private HttpServer server;
+    private NetworkListener listener;
+    public static final String localhost = "localhost";
+
+    public void runServer() throws IOException
+    {
+        runServer(defaultPort);
+    }
+
+    public void runServer(int port) throws IOException
+    {
+        logger.info("starting grizzly framework server on port {}", port);
+
+        ResourceConfig resourceConfig = new ResourceConfig(JerseyResource.class);
+        URI uri = UriBuilder.fromUri("http://" + localhost + ":" + port).build();
+        server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
+
+        listener = server.getListeners().iterator().next();
+        listener.setSecure(false);
+
+        NIOTransport nioTransport = TCPNIOTransportBuilder.newInstance()
+                .setReuseAddress(true)
+                .setIOStrategy(WorkerThreadIOStrategy.getInstance())
+                .setSelectorThreadPoolConfig(ThreadPoolConfig.defaultConfig()
+                        .setCorePoolSize(2)
+                        .setMaxPoolSize(4))
+                .setWorkerThreadPoolConfig(ThreadPoolConfig.defaultConfig()
+                        .setCorePoolSize(3)
+                        .setMaxPoolSize(20))
+                .build();
+        listener.setTransport((TCPNIOTransport)nioTransport);
+
+        server.addListener(listener);
+
+        final ServerConfiguration serverConfiguration = server.getServerConfiguration();
+        serverConfiguration.addHttpHandler(new HttpHandler()
+                                           {
+                                               public void service(Request request, Response response) throws Exception
+                                               {
+                                                   long startTime = new Date().getTime();
+                                                   final SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.UK);
+                                                   final String date = format.format(new Date(System.currentTimeMillis()));
+                                                   response.setContentType("text/plain");
+                                                   response.setContentLength(date.length());
+                                                   response.getWriter().write(date);
+                                                   dumpStats();
+                                                   logger.info("processed request in {} ms", new Date().getTime() - startTime);
+                                               }
+                                           }
+        );
+
+        server.start();
+
+        logger.info("bootstrap of grizzly framework server complete, running on http://{}:{}", localhost, port);
+    }
+
+    public void stopServer() throws IOException
+    {
+        logger.info("stopping grizzly framework server");
+
+        // Before shutdown dump thread info
+        dumpStats();
+
+        if (null != server)
+            server.shutdownNow();
+
+        logger.info("stopping of grizzly framework server complete");
+    }
+
+    private void dumpStats()
+    {
+        logger.info("available processors {}", Runtime.getRuntime().availableProcessors());
+        logger.info("server configuration name {}", server.getServerConfiguration().getName());
+        logger.info("listener io strategy {}", listener.getTransport().getIOStrategy());
+        if (null != listener.getTransport())
+        {
+            logger.info("server connection backlog {}", listener.getTransport().getServerConnectionBackLog());
+
+            logger.info("kernel thread pool queue {}/{} threads", listener.getTransport().getKernelThreadPoolConfig().getCorePoolSize(),
+                    listener.getTransport().getKernelThreadPoolConfig().getMaxPoolSize());
+            logger.info("kernel thread pool queue {}", listener.getTransport().getKernelThreadPoolConfig().getQueue());
+//            logger.info("kernel thread pool queue size {}", ((GrizzlyExecutorService)listener.getTransport().getKernelThreadPool()).getConfiguration().getQueue().size());
+
+            logger.info("worker thread pool queue {}/{} threads", listener.getTransport().getWorkerThreadPoolConfig().getCorePoolSize(),
+                    listener.getTransport().getWorkerThreadPoolConfig().getMaxPoolSize());
+            logger.info("worker thread pool queue {}", listener.getTransport().getWorkerThreadPoolConfig().getQueue());
+//            logger.info("worker thread pool queue size {}", ((GrizzlyExecutorService)listener.getTransport().getWorkerThreadPool()).getConfiguration().getQueue().size());
+        }
+        else
+        {
+            logger.warn("transport is NULL");
+        }
+
+    }
+}
---
+++ subprojects/playpens/java-playpen/web/grizzly-framework-playpen/src/test/java/com/fenixinfotech/grizzly/framework/playpen/JerseyGrizzlyFrameworkServerTest.java
@@ -0,0 +1,33 @@
+package com.fenixinfotech.grizzly.framework.playpen;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class JerseyGrizzlyFrameworkServerTest
+{
+    JerseyGrizzlyFrameworkServer grizzlyFrameworkServer;
+
+    @Before
+    public void init() throws IOException
+    {
+        grizzlyFrameworkServer = new JerseyGrizzlyFrameworkServer();
+        grizzlyFrameworkServer.runServer();
+    }
+
+    @After
+    public void tidyup() throws IOException
+    {
+        grizzlyFrameworkServer.stopServer();
+    }
+
+    @Test
+    public void testRunServer() throws Exception
+    {
+        // Add some verification
+//        Thread.sleep(3000);
+//        while(true) {}
+    }
+}
