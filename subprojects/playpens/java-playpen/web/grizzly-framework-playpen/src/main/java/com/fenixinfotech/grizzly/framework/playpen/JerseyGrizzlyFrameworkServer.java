7a61c2a04d7e6bec0bf4c195ee14ecbe2df6a4b1
steven.curtis@gmail.com <steven.curtis@gmail.com>
2016-06-10 09:27:49 +0100

    Use SSL on server and add spdy addon

--- subprojects/playpens/java-playpen/web/grizzly-framework-playpen/src/main/java/com/fenixinfotech/grizzly/framework/playpen/JerseyGrizzlyFrameworkServer.java
+++ subprojects/playpens/java-playpen/web/grizzly-framework-playpen/src/main/java/com/fenixinfotech/grizzly/framework/playpen/JerseyGrizzlyFrameworkServer.java
@@ -6,12 +6,12 @@ import org.glassfish.grizzly.http.server.*;
 import org.glassfish.grizzly.nio.NIOTransport;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
-import org.glassfish.grizzly.spdy.PushResource;
 import org.glassfish.grizzly.spdy.SpdyAddOn;
 import org.glassfish.grizzly.spdy.SpdyMode;
 import org.glassfish.grizzly.spdy.SpdyStream;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.grizzly.strategies.WorkerThreadIOStrategy;
-import org.glassfish.grizzly.threadpool.GrizzlyExecutorService;
 import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -19,7 +19,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.UriBuilder;
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,9 +31,9 @@ public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
 {
     private static final Logger logger = LoggerFactory.getLogger(JerseyGrizzlyFrameworkServer.class);
 
+    private URI uri;
     private HttpServer server;
     private NetworkListener listener;
-    public static final String localhost = "localhost";
 
     public void runServer() throws IOException
     {
@@ -43,28 +45,21 @@ public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
         logger.info("starting grizzly framework server on port {}", port);
 
         ResourceConfig resourceConfig = new ResourceConfig(JerseyResource.class);
-        URI uri = UriBuilder.fromUri("http://" + localhost + ":" + port).build();
-        server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
+        uri = UriBuilder.fromUri("https://localhost/").port(port).build();
+        server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig, true, createSSLContextConfigurator());
 
         listener = server.getListeners().iterator().next();
-        listener.setSecure(true);
 
         NIOTransport nioTransport = TCPNIOTransportBuilder.newInstance()
                 .setReuseAddress(true)
                 .setIOStrategy(WorkerThreadIOStrategy.getInstance())
-                .setSelectorThreadPoolConfig(ThreadPoolConfig.defaultConfig()
-                        .setCorePoolSize(2)
-                        .setMaxPoolSize(4))
-                .setWorkerThreadPoolConfig(ThreadPoolConfig.defaultConfig()
-                        .setCorePoolSize(3)
-                        .setMaxPoolSize(20))
+                .setSelectorThreadPoolConfig(ThreadPoolConfig.defaultConfig().setCorePoolSize(2).setMaxPoolSize(4))
+                .setWorkerThreadPoolConfig(ThreadPoolConfig.defaultConfig().setCorePoolSize(3).setMaxPoolSize(20))
                 .build();
         listener.setTransport((TCPNIOTransport)nioTransport);
 
-        SpdyAddOn spdyAddOn = new SpdyAddOn(SpdyMode.PLAIN);
+        SpdyAddOn spdyAddOn = new SpdyAddOn(SpdyMode.NPN);
         listener.registerAddOn(spdyAddOn);
-
-        server.addListener(listener);
 
         final ServerConfiguration serverConfiguration = server.getServerConfiguration();
         serverConfiguration.addHttpHandler(new HttpHandler()
@@ -72,21 +67,26 @@ public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
                                                public void service(Request request, Response response) throws Exception
                                                {
                                                    long startTime = new Date().getTime();
-//                                                   PushResource pushResource = PushResource.builder()
+
+                                                   // Put a short sleep in here so can see if requests queue up from browser
+                                                   Thread.currentThread().sleep(500);
+
                                                    // Get SPDY stream if it exists
                                                    final SpdyStream spdyStream = (SpdyStream) request.getAttribute(SpdyStream.SPDY_STREAM_ATTRIBUTE);
                                                    // if spdy stream is null it is not a SPDY based request
                                                    if (spdyStream != null)
                                                    {
-                                                       // push the content
-                                                        logger.info("found a SPDY stream");
+                                                       logger.info("found a SPDY stream");
+                                                   }
+                                                   else
+                                                   {
+                                                       logger.info("no SPDY stream available");
                                                    }
                                                    final SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.UK);
                                                    final String date = format.format(new Date(System.currentTimeMillis()));
                                                    response.setContentType("text/plain");
                                                    response.setContentLength(date.length());
                                                    response.getWriter().write(date);
-//                                                   dumpStats();
                                                    logger.info("processed request in {} ms", new Date().getTime() - startTime);
                                                }
                                            }
@@ -94,15 +94,32 @@ public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
 
         server.start();
 
-        logger.info("bootstrap of grizzly framework server complete, running on http://{}:{}", localhost, port);
+        logger.info("bootstrap of grizzly framework server complete, running on {}", uri);
     }
+
+    protected SSLEngineConfigurator createSSLContextConfigurator() throws MalformedURLException
+    {
+
+        SSLContextConfigurator sslContextConfigurator = new SSLContextConfigurator();
+        ClassLoader classLoader = getClass().getClassLoader();
+        sslContextConfigurator.setKeyStoreFile(new File("E:/Users/700608667/Development/GitHub/public-development/subprojects/playpens/java-playpen/web/grizzly-framework-playpen/src/main/resources/keystore.jks").toURI().toURL().getFile());
+        sslContextConfigurator.setKeyStorePass("changeit");
+
+        sslContextConfigurator.validateConfiguration(true);
+
+        SSLEngineConfigurator result = new SSLEngineConfigurator(
+                sslContextConfigurator.createSSLContext(),
+                false,
+                false,
+                false);
+        result.setClientMode(false);
+        return result;
+    }
+
 
     public void stopServer() throws IOException
     {
         logger.info("stopping grizzly framework server");
-
-        // Before shutdown dump thread info
-        dumpStats();
 
         if (null != server)
             server.shutdownNow();
@@ -110,29 +127,29 @@ public class JerseyGrizzlyFrameworkServer extends FrameworkServerBase
         logger.info("stopping of grizzly framework server complete");
     }
 
-    private void dumpStats()
+    public static void main(String[] args)
     {
-        logger.info("available processors {}", Runtime.getRuntime().availableProcessors());
-        logger.info("server configuration name {}", server.getServerConfiguration().getName());
-        logger.info("listener io strategy {}", listener.getTransport().getIOStrategy());
-        if (null != listener.getTransport())
+        JerseyGrizzlyFrameworkServer jerseyGrizzlyFrameworkServer = new JerseyGrizzlyFrameworkServer();
+        try
         {
-            logger.info("server connection backlog {}", listener.getTransport().getServerConnectionBackLog());
-
-            logger.info("kernel thread pool queue {}/{} threads", listener.getTransport().getKernelThreadPoolConfig().getCorePoolSize(),
-                    listener.getTransport().getKernelThreadPoolConfig().getMaxPoolSize());
-            logger.info("kernel thread pool queue {}", listener.getTransport().getKernelThreadPoolConfig().getQueue());
-//            logger.info("kernel thread pool queue size {}", ((GrizzlyExecutorService)listener.getTransport().getKernelThreadPool()).getConfiguration().getQueue().size());
-
-            logger.info("worker thread pool queue {}/{} threads", listener.getTransport().getWorkerThreadPoolConfig().getCorePoolSize(),
-                    listener.getTransport().getWorkerThreadPoolConfig().getMaxPoolSize());
-            logger.info("worker thread pool queue {}", listener.getTransport().getWorkerThreadPoolConfig().getQueue());
-//            logger.info("worker thread pool queue size {}", ((GrizzlyExecutorService)listener.getTransport().getWorkerThreadPool()).getConfiguration().getQueue().size());
+            jerseyGrizzlyFrameworkServer.runServer();
+            while (true)
+            {}
         }
-        else
+        catch (IOException e)
         {
-            logger.warn("transport is NULL");
+            logger.error("error thrown starting application", e);
         }
-
+        finally
+        {
+            try
+            {
+                jerseyGrizzlyFrameworkServer.stopServer();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
