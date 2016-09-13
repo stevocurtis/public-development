a7dc3a062dd514eabcc5b6d605e2fa145135968a
steven.curtis@gmail.com <steven.curtis@gmail.com>
2016-09-13 20:10:59 +0100

    Added skeleton of statefulj framework

---
+++ subprojects/playpens/java-playpen/fsm/statefulj-playpen/src/main/java/com/fenixinfotech/fsm/statefulj/StatefulJUtil.java
@@ -0,0 +1,9 @@
+package com.fenixinfotech.fsm.statefulj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StatefulJUtil
+{
+    private static final Logger logger = LoggerFactory.getLogger(StatefulJUtil.class);
+}
