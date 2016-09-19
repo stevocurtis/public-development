554446e2bacd2bc199f28274b8e0750f680050f0
steven.curtis@gmail.com <steven.curtis@gmail.com>
2016-09-19 13:19:30 +0100

    Added generated import classes

--- subprojects/playpens/java-playpen/xml/jaxb-playpen/src/main/java/com/fenixinfotech/jaxb/playpen/JAXBUtil.java
+++ subprojects/playpens/java-playpen/xml/jaxb-playpen/src/main/java/com/fenixinfotech/jaxb/playpen/JAXBUtil.java
@@ -1,5 +1,6 @@
 package com.fenixinfotech.jaxb.playpen;
 
+import com.fenixinfotech.jaxb.playpen.SampleRoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
--- subprojects/playpens/java-playpen/xml/jaxb-playpen/src/test/java/com/fenixinfotech/jaxb/playpen/JAXBUtilTest.java
+++ subprojects/playpens/java-playpen/xml/jaxb-playpen/src/test/java/com/fenixinfotech/jaxb/playpen/JAXBUtilTest.java
@@ -1,5 +1,7 @@
 package com.fenixinfotech.jaxb.playpen;
 
+import com.fenixinfotech.jaxb.playpen.SampleRoot;
+import com.fenixinfotech.jaxb.playpen.Samples;
 import org.junit.Before;
 import org.junit.Test;
 
