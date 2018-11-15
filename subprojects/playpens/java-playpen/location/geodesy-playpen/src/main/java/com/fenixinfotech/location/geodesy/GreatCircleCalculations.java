6041c55140d81fe83646cd571a2bec4d0715a1cc
steven.curtis@gmail.com <steven.curtis@gmail.com>
2018-11-15 00:13:40 +0000

    Added lat/long calcs and tests

--- subprojects/playpens/java-playpen/location/geodesy-playpen/src/main/java/com/fenixinfotech/location/geodesy/GreatCircleCalculations.java
+++ subprojects/playpens/java-playpen/location/geodesy-playpen/src/main/java/com/fenixinfotech/location/geodesy/GreatCircleCalculations.java
@@ -3,11 +3,43 @@ package com.fenixinfotech.location.geodesy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.lang.Math.PI;
+
 public class GreatCircleCalculations
 {
     private static final Logger logger = LoggerFactory.getLogger(GreatCircleCalculations.class);
+    public static final int EARTH_RADIUS_IN_METRES = 6366710;
 
-    public void run(Object... args) {
-        logger.info("in run method with args {}", args);
+    /**
+     * The number of kilometers per degree of latitude is approximately the same at all locations, approx
+     *
+     * (2*pi/360) * r_earth = 111 km / degree
+     *
+     * @param distanceMetres
+     * @param latitudeDegrees
+     * @return Longitude Offset in Degrees
+     */
+    public static double getLatitudeOffset(double distanceMetres, double latitudeDegrees) {
+        double latitudeOffset = Math.abs(latitudeDegrees  + (distanceMetres / EARTH_RADIUS_IN_METRES) * (180 / PI));
+        logger.debug("found latitudeOffset {} for distanceMetres {} from latitudeDegrees {}", latitudeOffset, distanceMetres, latitudeDegrees);
+        return latitudeOffset;
+    }
+
+    /**
+     * The number of kilometers per degree of longitude is approximately
+     *
+     * (pi/180) * r_earth * cos(theta)
+     *
+     * Where theta is the latitude in degrees.
+     *
+     * @param distanceMetres
+     * @param latitudeDegrees
+     * @param longitudeDegrees
+     * @return Latitude Offset in Degrees
+     */
+    public static double getLongitudeOffset(double distanceMetres, double latitudeDegrees, double longitudeDegrees) {
+        double longitudeOffset = Math.abs(longitudeDegrees + (distanceMetres / EARTH_RADIUS_IN_METRES) * (180 / PI) / Math.cos(Math.toRadians(latitudeDegrees)));
+        logger.debug("found longitudeOffset {} for distanceMetres {} from latitudeDegrees {} longitudeDegrees {}", longitudeOffset, distanceMetres, latitudeDegrees, longitudeDegrees);
+        return longitudeOffset;
     }
 }
--- subprojects/playpens/java-playpen/location/geodesy-playpen/src/test/java/com/fenixinfotech/location/geodesy/GreatCircleCalculationsTest.java
+++ subprojects/playpens/java-playpen/location/geodesy-playpen/src/test/java/com/fenixinfotech/location/geodesy/GreatCircleCalculationsTest.java
@@ -6,7 +6,44 @@ import static org.junit.Assert.*;
 
 public class GreatCircleCalculationsTest {
 
+    private static final double BERLIN_LATITUDE = 52.507950;
+    private static final double BERLIN_LONGITUDE = 13.408941;
+    private static final double BRASILIA_LATITUDE = -15.786499;
+    private static final double BRASILIA_LONGITUDE = -47.870515;
+    private static final double DUBLIN_LATITUDE = 53.341545;
+    private static final double DUBLIN_LONGITUDE = -6.254362;
+    private static final double GREENWICH_LATITUDE = 51.482211;
+    private static final double GREENWICH_LONGITUDE = 0;
+    private static final double GULF_OF_GUINEA_LATITUDE = 0;
+    private static final double GULF_OF_GUINEA_LONGITUDE = 0;
+    private static final double WASHINGTON_LATITUDE = 38.900688;
+    private static final double WASHINGTON_LONGITUDE = -77.033640;
+
+    private static final double DEFAULT_DISTANCE_METRES = 500;
     @Test
-    public void run() {
+    public void getHorizontalLatitudeRange() {
+        // BERLIN
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, BERLIN_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, BERLIN_LATITUDE, BERLIN_LONGITUDE);
+
+        // BRASILIA
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, BRASILIA_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, BRASILIA_LATITUDE, BRASILIA_LONGITUDE);
+
+        // DUBLIN
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, DUBLIN_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, DUBLIN_LATITUDE, DUBLIN_LONGITUDE);
+
+        // GULF_OF_GUINEA
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, GULF_OF_GUINEA_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, GULF_OF_GUINEA_LATITUDE, GULF_OF_GUINEA_LONGITUDE);
+
+        // GREENWICH
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, GREENWICH_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, GREENWICH_LATITUDE, GREENWICH_LONGITUDE);
+
+        // WASHINGTON
+        GreatCircleCalculations.getLatitudeOffset(DEFAULT_DISTANCE_METRES, WASHINGTON_LATITUDE);
+        GreatCircleCalculations.getLongitudeOffset(DEFAULT_DISTANCE_METRES, WASHINGTON_LATITUDE, WASHINGTON_LONGITUDE);
     }
 }
